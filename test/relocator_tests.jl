c6deaef5ff2389d606a3f475516d1ee0d129bfce
Hatsune Miku <999Hatsune@gmail.com>
2016-07-15 10:40:20 +0900

    modify relocator_tests.jl for test when ./dll does not exist

--- test/relocator_tests.jl
+++ test/relocator_tests.jl
@@ -4,24 +4,38 @@ import Relocator
 import Relocator: _mf
 using Base.Test
 
+function testHere(r, p::Array{UInt8,1})
+  m = sizeof(p)
+  # println(m)
+  ccall(_mf(r, :_dummy, :here), Ptr{Cchar}, (Ptr{Cchar}, Cint,), pointer(p), m)
+end
+
+function checkPtrCchar(r, p::Array{UInt8,1}, a::Array{ASCIIString,1})
+  testHere(r, p)
+  println(bytestring(p))
+  for s in a
+    if ccall(:strcmp, Cint, (Ptr{Cchar}, Ptr{Cchar},),
+      pointer(p), pointer(s.data)) == 0
+      return true
+    end
+  end
+  return false
+end
+
 println("Testing Relocator")
 
 println("1")
 r1 = Relocator.Rel()
 Relocator._init(r1, [:_dummy], "")
 p1 = "_dummy_9b".data
-ccall(_mf(r1, :_dummy, :here), Ptr{Cchar}, (Ptr{Cchar}, Cint,), pointer(p1), 9)
-println(bytestring(p1))
-@test bytestring(p1[1:4]) == "dll\0"
+@test checkPtrCchar(r1, p1, ["dll", "test", "prjroot"])
 Relocator._close(r1)
 
 println("2")
 r2 = Relocator.Rel()
 Relocator._init(r2, [:_dummy], ".")
 p2 = "_dummy_9b".data
-ccall(_mf(r2, :_dummy, :here), Ptr{Cchar}, (Ptr{Cchar}, Cint,), pointer(p2), 9)
-println(bytestring(p2))
-@test bytestring(p2[1:5]) == "test\0" || bytestring(p1[1:4]) == "dll\0"
+@test checkPtrCchar(r2, p2, ["dll", "test", "prjroot"])
 Relocator._close(r2)
 
 println("3")
@@ -29,9 +43,7 @@ r3 = Relocator.Rel()
 try
   Relocator._init(r3, [:_dummy], "..")
   p3 = Array{UInt8,1}([49,50,51,52,53,54,55,56,57])
-  ccall(_mf(r3, :_dummy, :here), Ptr{Cchar}, (Ptr{Cchar}, Cint,), pointer(p3), 9)
-  println(bytestring(p3))
-  @test bytestring(p3[1:4]) == "dll\0"
+  @test checkPtrCchar(r3, p3, ["dll", "prjroot"]) # without "test"
 catch err
   println(err) # ArgumentError("not found module ':_dummy'")
 finally
